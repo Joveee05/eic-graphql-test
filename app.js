8ec3499a14323310d0276a22fc4cb8649e713f51
Brian Etaghene <joveee05@gmail.com>
2023-02-06 18:04:14 +0100

    [Added] Error Handler && API Key Verification

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql').graphqlHTTP;
 const schema = require('./schema/schema');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 const DB = require('./database');
 const app = express();
+const authController = require('./controllers/auth');
 
 dotenv.config({ path: './config.env' });
+
+const router = express.Router();
 
 app.use(
   '/graphql',
@@ -21,6 +25,29 @@ app.use(cors());
 DB.authenticate()
   .then(() => console.log('Database Connected...'))
   .catch((err) => console.log('Error: ' + err));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(cookieParser());
+
+app.use(router);
+router.use(authController.verifyApiKey);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 app.listen(5000, () => {
   console.log('API running on port 5000');
