b8363c56e4a1b4834d31092a6cecbd2aab27ea0c
Brian Etaghene <joveee05@gmail.com>
2023-02-08 19:22:07 +0100

    [Updated] Error handler

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const graphqlHTTP = require('express-graphql').graphqlHTTP;
 const schema = require('./schema/schema');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
 const dotenv = require('dotenv');
 const DB = require('./database');
 const app = express();
@@ -11,6 +12,7 @@ const authController = require('./controllers/auth');
 dotenv.config({ path: './config.env' });
 
 const router = express.Router();
+app.use(router);
 
 app.use(
   '/graphql',
@@ -22,6 +24,10 @@ app.use(
 
 app.use(cors());
 
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 DB.authenticate()
   .then(() => console.log('Database Connected...'))
   .catch((err) => console.log('Error: ' + err));
@@ -29,9 +35,6 @@ DB.authenticate()
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 app.use(cookieParser());
-
-app.use(router);
-router.use(authController.verifyApiKey);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -46,7 +49,10 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({
+    message: err.message,
+    error: err,
+  });
 });
 
 app.listen(5000, () => {
